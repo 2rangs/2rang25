7220e723795e752e105ee3b294745e3976d9d198
2rangs <2_rang25@naver.com>
2024-03-05 10:37:41 +0900

    google Ad 메타 태그 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,13 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@nuxt/ui', '@pinia/nuxt',],
   css: [ '/public/assets/main.scss'],
-  //@ts-ignore
-  head: {
-    title: '2rang25',
-    meta: [
-      { charset: 'utf-8' },
-      { name: '"google-adsense-account', content: 'ca-pub-5843730547937550' }
-    ]
+  app: {
+    head: {
+      title: '2rang25',
+      meta: [
+        { charset: 'utf-8' },
+        { name: '"google-adsense-account', content: 'ca-pub-5843730547937550' }
+      ]
+    }
   }
 })
