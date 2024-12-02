00140a79674f0de413d1bb2b781489085abad28c
2rangs <2_rang25@naver.com>
2024-12-02 18:35:29 +0900

    map 함수 조건 설정

--- utils/commons.ts
+++ utils/commons.ts
@@ -3,16 +3,18 @@ import {categories} from "@vueuse/metadata";
 
 export const getCategoryName = (id: number) => {
     const categryName = ref()
-    const categories = ref(JSON.parse(localStorage.getItem('categories') as string))
-    categories.value.map((root : any) => {
-        root.children.map((tree : any) => {
-            tree.children.map((child : any) => {
-                if(parseInt(child._id) === id){
-                    categryName.value = child.title.split(' (')[0]
-                }
+    const categories : any = JSON.parse(localStorage.getItem('categories') as string)
+    if(categories){
+        categories.map((root : any) => {
+            root.children.map((tree : any) => {
+                tree.children.map((child : any) => {
+                    if(parseInt(child._id) === id){
+                        categryName.value = child.title.split(' (')[0]
+                    }
+                })
             })
         })
-    })
+    }
     return categryName.value
 };
 
