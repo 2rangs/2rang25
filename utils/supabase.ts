5b65c9bc0ce2afa2e6609d3069c27ea3a109a6a4
2rangs <2_rang25@naver.com>
2024-12-20 16:13:11 +0900

    fixed : env 수정

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,7 @@
 
 import {createBrowserClient} from "@supabase/ssr";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey =  import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = process.env.VITE_SUPABASE_URL as string
+const supabaseKey =  process.env.VITE_SUPABASE_KEY as string
 export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
         
