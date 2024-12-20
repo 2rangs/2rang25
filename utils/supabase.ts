7652a40821a1285ad6605666408be1d1adab8128
2rangs <2_rang25@naver.com>
2024-12-20 16:53:25 +0900

    fixed : 500 수정 테스트
    실 운영환경에서는 계속 오류만 뜸

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,7 @@
 
-import {createBrowserClient} from "@supabase/ssr";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL as string
-const supabaseKey =  process.env.VITE_SUPABASE_KEY as string
-export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
-        
+
+const supabaseUrl = process.env.VITE_SUPABASE_URL || 'https://wbtbmarooytomtynuvoe.supabase.co'
+const supabaseKey =  process.env.VITE_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndidGJtYXJvb3l0b210eW51dm9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg2NTQ3NTAsImV4cCI6MjAxNDIzMDc1MH0.xV176B7fRp8qooWmygkUmGxA4shPEjH7JGXNrsx3QEk'
+export const supabase = createClient(supabaseUrl, supabaseKey);
